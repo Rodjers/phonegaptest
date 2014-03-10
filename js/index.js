f78388fcc924cf09a33f2720a5fa71cb12211b4a
Oddgeir Gitlestad <oddgeir.gitlestad@gitlestadit.no>
2014-03-10 13:10:26 +0100

    Debug

--- js/index.js
+++ js/index.js
@@ -33,6 +33,13 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        
+console.log(JSON.stringify(this);
+console.log(JSON.stringify(cordova);
+var bluetoothSerial = cordova.require('bluetoothSerial');
+
+console.log(bluetooothSerial);
+        bluetoothSerial.connect("", function(){alert("Success")}, function(){alert("Fail")});
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
